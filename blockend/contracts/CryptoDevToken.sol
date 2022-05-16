ba10927f7c92f2a167664a9f8a41e9bf3673e6f6
Alex Johnson <ajohnson2389@gmail.com>
2022-05-16 14:12:26 -0400

    Created mint functionality

--- blockend/contracts/CryptoDevToken.sol
+++ blockend/contracts/CryptoDevToken.sol
@@ -8,9 +8,10 @@ import "./ICryptoDevs.sol";
 contract CryptoDevToken is ERC20, Ownable {
     // Price of one Crypto Dev token
     uint256 public constant tokenPrice = 0.001 ether;
-    // Each NFT would give the user 10 tokens
+    // Each NFT would give the owner 10 tokens
     uint256 public constant tokensPerNFT = 10 * 10**18;
-
+    // Maximum total supply is 10000 for Crypto Dev Tokens
+    uint256 public constant maxTotalSupply = 10000 * 10**18;
     // CryptoDevsNFT contract instance
     ICryptoDevs CryptoDevsNFT;
 
@@ -20,4 +21,29 @@ contract CryptoDevToken is ERC20, Ownable {
     constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
         CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);
     }
+
+    /**
+     * @dev Mints `amount` number of CryptoDevTokens
+     * @notice `msg.value` should be equal or greater than the tokenPrice * amount
+     */
+    function mint(uint256 amount) public payable {
+        // the value of ETH that should be equal or greater than the tokenPrice * amount
+        uint256 _requiredAmount = tokenPrice * amount;
+        require(msg.value >= _requiredAmount, "Ether sent is incorrect");
+        // total tokens + amount <= 10000, otherwise revert the transaction
+        uint256 amountWithDecimals = amount * 10**18;
+        require(
+            (totalSupply() + amountWithDecimals) <= maxTotalSupply,
+            "Exceeds the max total supply available."
+        );
+        // call the internal _mint function from @openzeppelin
+        _mint(msg.sender, amountWithDecimals);
+    }
+
+    /**
+     * @dev Mints tokens based on the number of NFT's held by the sender
+     * @notice balance of Crypto Dev NFT's owned by the sender should be greater than 0
+     * Tokens should not have been claimed for all the NFTs owned by the sender
+     */
+    function claim() public {}
 }
