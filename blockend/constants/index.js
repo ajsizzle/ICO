87742ace979ff66984e92380345eda7c25724f19
Alex Johnson <ajohnson2389@gmail.com>
2022-05-16 15:28:28 -0400

    set up deploy script for rinkeby

---
+++ blockend/constants/index.js
@@ -0,0 +1,4 @@
+// Address of CryptoDev NFT Collection
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
---
+++ blockend/scripts/deploy.js
@@ -0,0 +1,28 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+async function main() {
+  // reference address of NFT contract
+  const cryptoDevsNFTContract = CRYPTO_DEVS_NFT_CONTRACT_ADDRESS;
+  // set up address of token contract for deployment
+  const cryptoDevsTokenContract = await ethers.getContractFactory(
+    "CryptoDevToken"
+  );
+  // deploy the contract
+  const deployedCryptoDevsTokenContract = await cryptoDevsTokenContract.deploy(
+    cryptoDevsNFTContract
+  );
+  // print address to console
+  console.log(
+    "Crypto Devs Token Contract Address:",
+    deployedCryptoDevsTokenContract.address
+  );
+}
+
+// call main()
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
